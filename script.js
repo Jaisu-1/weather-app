bc665ad91b85de6037ef99d1bf069017b5669a55
Jaisurya Nanduri <jaisunanduri1@gmail.com>
2018-10-10 18:11:42 +0530

    Added pics

--- script.js
+++ script.js
@@ -24,25 +24,25 @@ function init(resultFromServer){
     switch(resultFromServer.weather[0].main) {
         case 'Fog':
         case 'Clear':
-            document.body.style.backgroundImage = 'url("pics/clear.jpg")'
+            document.body.style.backgroundImage = 'url("../weather-app/pics/clear.jpg")'
             break;
 
         case 'Clouds':
-            document.body.style.backgroundImage = 'url("pics/cloudy.jpg")'
+            document.body.style.backgroundImage = 'url("../weather-app/pics/cloudy.jpg")'
             break;
 
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
-            document.body.style.backgroundImage = 'url("pics/rain.jpg")'
+            document.body.style.backgroundImage = 'url("../weather-app/pics/rain.jpg")'
             break;
 
         case 'Thunderstorm':
-            document.body.style.backgroundImage = 'url("pics/storm.jpg")'
+            document.body.style.backgroundImage = 'url("../weather-app/pics/storm.jpg")'
             break;
 
         case 'Snow':
-            document.body.style.backgroundImage = 'url("pics/snow.jpg")'
+            document.body.style.backgroundImage = 'url("../weather-app/pics/snow.jpg")'
             break;
 
         default:
